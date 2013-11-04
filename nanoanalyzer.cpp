7c9320d595b03971251f735ebbe4782981f5ed63
mskubenich <misha.skubenich@gmail.com>
2013-11-04 15:26:05 +0200

    read file

--- nanoanalyzer.cpp
+++ nanoanalyzer.cpp
@@ -2,6 +2,9 @@
 #include <QMouseEvent>
 #include <qmessagebox.h>
 #include <qfiledialog.h>
+#include <qstandarditemmodel.h>
+#include <qtextstream.h>
+
 
 NanoAnalyzer::NanoAnalyzer(QWidget *parent)
     : QMainWindow(parent)
@@ -19,5 +22,31 @@ void NanoAnalyzer::on_startButton_clicked()
 	QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"),
 	                                                 "",
 	                                                 tr("Files (*.*)"));
-	QMessageBox::information(this, "Hello World!", fileName);
+
+	QStandardItemModel *model = new QStandardItemModel(200,3,this);
+	model->setHorizontalHeaderItem(0, new QStandardItem(QString("x")));
+	model->setHorizontalHeaderItem(1, new QStandardItem(QString("y")));
+	model->setHorizontalHeaderItem(2, new QStandardItem(QString("z")));
+
+	QFile file(fileName);
+	if(!file.open(QIODevice::ReadOnly)) {
+	    QMessageBox::information(0, "error", file.errorString());
+	}
+
+	QTextStream in(&file);
+
+	int i = 0;
+	while(!in.atEnd()) {
+	    QString line = in.readLine();
+//	    QStringList fields = line.split(",");
+//	    model->appendRow(fields);
+
+		QStandardItem *firstRow = new QStandardItem(QString(line));
+		model->setItem(i,0,firstRow);
+		i++;
+	}
+
+	file.close();
+
+	ui.tableView->setModel(model);
 }
